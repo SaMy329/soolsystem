21d045027d84794a1372e52c7e90432542d33dd0
SaMy329 <59095473+SaMy329@users.noreply.github.com>
2020-10-10 21:36:39 +0200

    Update and rename bot.js to server.js

--- server.js
+++ server.js
@@ -4808,4 +4808,4 @@ client.on('message',  (message) => {
 
 
 
-client.login('NzMxODcxMzc4ODMxMTc5Nzg2.XwsWVg.V40YHt4yCjcZcbszSnh04BVDck4'); 
+client.login('NzY0NDg2NDQxMTI0MjMzMjg3.X4G9ew.1kIB6nNz-YC3hmiA8E3ey0pwhtU'); 
